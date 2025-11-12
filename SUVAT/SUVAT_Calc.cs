463b3a9353e6ed09b5d6ec347ee6441c65e825e1
Slsickslider <georgiorocco@outlook.com>
2025-11-12 23:54:06 +0000

    Initial commit: SUVAT project

---
+++ SUVAT/SUVAT_Calc.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Project
+{
+    class UI
+    {
+        public static void Main()
+        {
+            bool Running = true;
+
+            Console.WriteLine("Welcome to the SUVAT Calculator!");
+            while (Running)
+            {
+                Console.WriteLine("------------------------------");
+                Running = false;
+            }
+        }
+        
+    }
+
+    class Equations
+    {
+        private float S;
+        private float U;
+        private float V;
+        private float A;
+        private float T;
+        private float result;
+
+        public Equations(float s, float u, float v, float a, float t)
+        {
+            S = s;
+            U = u;
+            V = v;
+            A = a;
+            T = t;
+        }
+
+        public void WhichEquation()
+        {
+            
+        }
+    }
+}
